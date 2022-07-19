9e961aca15259d3424ae5f6cb2e53af0ef3369bb
Collins Oriakhi <collins.oriakhi@itara.co>
2022-07-19 21:47:36 +0100

    Corrected Naming issues on the sol files

--- contracts/BlockTodos.sol
+++ contracts/BlockTodos.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-contract SolidTodos{ 
+contract BlockTodos{ 
     uint public taskCount;
     struct Task { 
         string taskName;
---
+++ migrations/2_deploy_contacts.js
@@ -0,0 +1,5 @@
+const BlockTodos = artifacts.require("BlockTodos");
+
+module.exports = function (deployer) {
+  deployer.deploy(BlockTodos);
+};
